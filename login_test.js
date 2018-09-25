2a53b48ee6b5065e6b55bf0e3d3dc6e6de653978
user <us3r>
2018-09-25 15:34:00 +0700

    using Page Models

--- login_test.js
+++ login_test.js
@@ -1,25 +1,15 @@
-const usernameField = '#monitoring_analytic.cartenz.com:id/etUsername'
-const usernameValue = 'flocs'
-const usernameValueInvalid = 'asdf'
-const passwordField = '#monitoring_analytic.cartenz.com:id/etPassword'
-const passwordValue = 'coflocs!'
-const passwordValueInvalid = 'asdf!'
-const loginButton = '#monitoring_analytic.cartenz.com:id/btnLogin'
-const homeHeaderField = '//android.widget.TextView[@index=2]'
-const homeHeaderValue = 'Tax Analytics'
-
 Feature('Login');
 
-Scenario('Login with Valid Credentials', (I) => {
-	I.fillField(usernameField, usernameValue)
-	I.fillField(passwordField, passwordValue)
-	I.click(loginButton)
-	I.see(homeHeaderValue, homeHeaderField)
+Scenario('Login with Valid Credentials', (I, loginPage) => {
+	I.fillField(loginPage.field.usernameField, loginPage.value.usernameValue)
+	I.fillField(loginPage.field.passwordField, loginPage.value.passwordValue)
+	I.click(loginPage.button.loginButton)
+	I.see(loginPage.value.homeHeaderValue, loginPage.field.homeHeaderField)
 });
 
-Scenario('Login with Invalid Credentials', (I) => {
-	I.fillField(usernameField, usernameValueInvalid)
-	I.fillField(passwordField, passwordValueInvalid)
-	I.click(loginButton)
-	I.dontSee(homeHeaderField)
+Scenario('Login with Invalid Credentials', (I, loginPage) => {
+	I.fillField(loginPage.field.usernameField, loginPage.value.usernameValueInvalid)
+	I.fillField(loginPage.field.passwordField, loginPage.value.passwordValueInvalid)
+	I.click(loginPage.button.loginButton)
+	I.dontSee(loginPage.field.homeHeaderField)
 });
---
+++ pages/login.js
@@ -0,0 +1,24 @@
+
+const I = actor();
+
+module.exports = {
+
+  // insert your locators and methods here
+  field: {
+  	usernameField : '#monitoring_analytic.cartenz.com:id/etUsername',
+  	passwordField : '#monitoring_analytic.cartenz.com:id/etPassword',
+  	homeHeaderField : '//android.widget.TextView[@index=2]'
+  },
+
+  button: {
+  	loginButton : '#monitoring_analytic.cartenz.com:id/btnLogin'
+  },
+
+  value: {
+  	usernameValue : 'flocs',
+  	usernameValueInvalid : 'asdf',
+  	passwordValue : 'coflocs!',
+  	passwordValueInvalid : 'asdf!',
+  	homeHeaderValue : 'Tax Analytics'
+  }
+}
