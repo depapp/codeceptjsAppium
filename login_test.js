46027237831dd9f0267f37ce3d70c49a2d2bfcbb
user <us3r>
2018-09-26 12:39:03 +0700

    code beautifier

--- login_test.js
+++ login_test.js
@@ -1,15 +1,15 @@
 Feature('Login');
 
 Scenario('Login with Valid Credentials', (I, loginPage) => {
-	I.fillField(loginPage.field.usernameField, loginPage.value.usernameValue)
-	I.fillField(loginPage.field.passwordField, loginPage.value.passwordValue)
-	I.click(loginPage.button.loginButton)
-	I.see(loginPage.value.homeHeaderValue, loginPage.field.homeHeaderField)
+    I.fillField(loginPage.field.usernameField, loginPage.value.usernameValue)
+    I.fillField(loginPage.field.passwordField, loginPage.value.passwordValue)
+    I.click(loginPage.button.loginButton)
+    I.see(loginPage.value.homeHeaderValue, loginPage.field.homeHeaderField)
 });
 
 Scenario('Login with Invalid Credentials', (I, loginPage) => {
-	I.fillField(loginPage.field.usernameField, loginPage.value.usernameValueInvalid)
-	I.fillField(loginPage.field.passwordField, loginPage.value.passwordValueInvalid)
-	I.click(loginPage.button.loginButton)
-	I.dontSee(loginPage.field.homeHeaderField)
+    I.fillField(loginPage.field.usernameField, loginPage.value.usernameValueInvalid)
+    I.fillField(loginPage.field.passwordField, loginPage.value.passwordValueInvalid)
+    I.click(loginPage.button.loginButton)
+    I.dontSee(loginPage.field.homeHeaderField)
 });
--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,23 @@
-
 const I = actor();
 
 module.exports = {
 
-  // insert your locators and methods here
-  field: {
-  	usernameField : '#monitoring_analytic.cartenz.com:id/etUsername',
-  	passwordField : '#monitoring_analytic.cartenz.com:id/etPassword',
-  	homeHeaderField : '//android.widget.TextView[@index=2]'
-  },
+    // insert your locators and methods here
+    field: {
+        usernameField: '#monitoring_analytic.cartenz.com:id/etUsername',
+        passwordField: '#monitoring_analytic.cartenz.com:id/etPassword',
+        homeHeaderField: '//android.widget.TextView[@index=2]'
+    },
 
-  button: {
-  	loginButton : '#monitoring_analytic.cartenz.com:id/btnLogin'
-  },
+    button: {
+        loginButton: '#monitoring_analytic.cartenz.com:id/btnLogin'
+    },
 
-  value: {
-  	usernameValue : 'flocs',
-  	usernameValueInvalid : 'asdf',
-  	passwordValue : 'coflocs!',
-  	passwordValueInvalid : 'asdf!',
-  	homeHeaderValue : 'Tax Analytics'
-  }
+    value: {
+        usernameValue: 'flocs',
+        usernameValueInvalid: 'asdf',
+        passwordValue: 'coflocs!',
+        passwordValueInvalid: 'asdf!',
+        homeHeaderValue: 'Tax Analytics'
+    }
 }
